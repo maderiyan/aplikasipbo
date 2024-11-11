b5999a553752533085017cd764b00268db329ba2
maderiyan <made.riyan.adi@gmail.com>
2024-11-11 12:39:01 +0800

    Init Project

---
+++ src/aplikasipbo/Aplikasipbo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasipbo;
+
+/**
+ *
+ * @author mader
+ */
+public class Aplikasipbo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
